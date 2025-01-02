1101a04575c341f17320cf808185e6b3192ac0bd
Vadim-Kurnatovskiy <vadim.kurnatovskiy@flatstack.com>
2025-01-02 17:22:41 +0300

    v 2.0

--- old-bot.py
+++ old-bot.py
@@ -1,37 +1,20 @@
-import asyncio
-import logging
 import os
+import logging
 import random
-import uvicorn
 import psycopg2
-
-from starlette.applications import Starlette
-from starlette.requests import Request
-from starlette.responses import PlainTextResponse, Response
-from starlette.routing import Route
-
 from telegram import Update, InputFile, InlineKeyboardButton, InlineKeyboardMarkup, BotCommand
-from telegram.ext import Application, CommandHandler, MessageHandler, CallbackQueryHandler, filters, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, CallbackQueryHandler, filters, ContextTypes
 
-# Enable logging
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
-logging.getLogger("httpx").setLevel(logging.WARNING)
-
+# Настройки логгирования
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Define configuration constants
-URL = os.getenv("RENDER_EXTERNAL_URL")
-PORT = 8000
-TOKEN = os.getenv("TOKEN")
-
+# Подключение к базе данных
 DB_HOST = os.getenv("DB_HOST")
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-
-WELCOME_TEXT = "Добро пожаловать в редакцию Владожского Вестика! Мы рады приветствовать новых сплетников и сплетниц в наших рядах!"
+TOKEN = os.getenv("TOKEN")
 INSTRUCTION_TEXT = """Как писать сплетню?
 
 В бот необязательно отправлять готовую для публикации сплетню, редакция «Владожского вестника» может докрутить ее на основе полученной от вас информации.
@@ -92,67 +75,6 @@ RESPONSES = [
     "Отличный вклад в великое дело лагерной гласности!"
 ]
 
-MOUSE_OF_DAY_RESPONSES = [
-  "https://ibb.co/YTfx2Jq",
-  "https://ibb.co/fFdwJQ4",
-  "https://ibb.co/4jc9gP0",
-  "https://ibb.co/0JJwDLT",
-  "https://ibb.co/wyHf90X",
-  "https://ibb.co/9rhMwc0",
-  "https://ibb.co/myYzXpQ",
-  "https://ibb.co/S3MPbt4",
-  "https://ibb.co/k5WgdKC",
-  "https://ibb.co/2q8t9dT",
-  "https://ibb.co/JC341ML",
-  "https://ibb.co/k48qDf1",
-  "https://ibb.co/MgB5Hgz",
-  "https://ibb.co/w0XXZds",
-  "https://ibb.co/9g8c5Yp",
-  "https://ibb.co/SBFZMpf",
-  "https://ibb.co/s3ncQ7g",
-  "https://ibb.co/4JFVR0K",
-  "https://ibb.co/vhz2W0d",
-  "https://ibb.co/p3cVLSg",
-  "https://ibb.co/ZmS0RR2",
-  "https://ibb.co/7CfH2w8",
-  "https://ibb.co/W024mPX",
-  "https://ibb.co/tsz9xbX",
-  "https://ibb.co/yk5w4FH",
-  "https://ibb.co/pn29L6X",
-  "https://ibb.co/M9Gh9mq",
-  "https://ibb.co/SJrq0V5",
-  "https://ibb.co/n7qJ6g8",
-  "https://ibb.co/qsSSTRJ",
-  "https://ibb.co/qk3WXsk",
-  "https://ibb.co/n1yDxPY",
-  "https://ibb.co/XVFCnS1",
-  "https://ibb.co/M50bjDC",
-  "https://ibb.co/c179WDM",
-  "https://ibb.co/hW1RTjQ",
-  "https://ibb.co/rcS7bGz",
-  "https://ibb.co/jzLs8zY",
-  "https://ibb.co/CJgG0J6",
-  "https://ibb.co/wzrhWVY",
-  "https://ibb.co/NSX6xh3",
-  "https://ibb.co/p3GFjTp",
-  "https://ibb.co/CMSMKNv",
-  "https://ibb.co/48QVFHV",
-  "https://ibb.co/k3xg1pd",
-  "https://ibb.co/1GK35PR",
-  "https://ibb.co/LPFNZVh",
-  "https://ibb.co/98V4TPv",
-  "https://ibb.co/Ln8zVND",
-  "https://ibb.co/zXFXdds",
-  "https://ibb.co/LSBV6fG",
-  "https://ibb.co/VMCt45F",
-  "https://ibb.co/f2cn8HF",
-  "https://ibb.co/kK9c2z7",
-  "https://ibb.co/hcMLrbK",
-  "https://ibb.co/J3NBYYS",
-  "https://ibb.co/0C1jLb6"
-]
-
-
 def init_db():
     conn = psycopg2.connect(host=DB_HOST, database=DB_NAME, user=DB_USER, password=DB_PASSWORD)
     cursor = conn.cursor()
@@ -169,7 +91,6 @@ def init_db():
     cursor.close()
     conn.close()
 
-
 # Сохранение сообщения в базу данных
 def save_message(username, message_text, file_path=None):
     conn = psycopg2.connect(host=DB_HOST, database=DB_NAME, user=DB_USER, password=DB_PASSWORD)
@@ -182,24 +103,24 @@ def save_message(username, message_text, file_path=None):
 def sample_response():
   return random.choice(RESPONSES)
 
-def sample_mouse_of_day_response():
-  return random.choice(MOUSE_OF_DAY_RESPONSES)
+async def set_commands(application: ApplicationBuilder):
+    await application.bot.set_my_commands([
+        BotCommand("start","Добро пожаловать"),
+        BotCommand("rules","Руководство")
+      ])
 
 # Обработчик команды /start
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.effective_user
     keyboard = [
         [InlineKeyboardButton("Руководство", callback_data="rules")]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    await update.message.reply_text(WELCOME_TEXT, reply_markup=reply_markup)
+    await update.message.reply_text(f"Привет, {user.first_name}!", reply_markup=reply_markup)
 
 # Обработчик команды /rules
 async def rules_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(INSTRUCTION_TEXT)
-
-# Обработчик команды /rules
-async def mouse_of_day_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(sample_mouse_of_day_response())
 
 # Обработчик текстовых сообщений
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -233,56 +154,19 @@ async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if query.data == "rules":
         await query.edit_message_text(INSTRUCTION_TEXT)
 
+# Основной запуск бота
+if __name__ == "__main__":
+    init_db()
 
-###
+    # Создание приложения
+    application = ApplicationBuilder().token(TOKEN).build()
 
-async def main() -> None:
-    """Set up PTB application and a web application for handling the incoming requests."""
-    application = Application.builder().token(TOKEN).updater(None).build()
-
-    # register handlers
+    # Обработчики
     application.add_handler(CommandHandler("start", start_command))
     application.add_handler(CommandHandler("rules", rules_command))
-    application.add_handler(CommandHandler("mouse_of_day", mouse_of_day_command))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     application.add_handler(MessageHandler(filters.Document.ALL | filters.PHOTO | filters.VIDEO, handle_file))
     application.add_handler(CallbackQueryHandler(button_handler))
 
-    # Pass webhook settings to telegram
-    await application.bot.set_webhook(url=f"{URL}/telegram", allowed_updates=Update.ALL_TYPES)
-
-    # Set up webserver
-    async def telegram(request: Request) -> Response:
-        """Handle incoming Telegram updates by putting them into the `update_queue`"""
-        await application.update_queue.put(
-            Update.de_json(data=await request.json(), bot=application.bot)
-        )
-        return Response()
-
-    async def health(_: Request) -> PlainTextResponse:
-        return PlainTextResponse(content="OK")
-
-    starlette_app = Starlette(
-        routes=[
-            Route("/telegram", telegram, methods=["POST"]),
-            Route("/healthcheck", health, methods=["GET"]),
-        ]
-    )
-    webserver = uvicorn.Server(
-        config=uvicorn.Config(
-            app=starlette_app,
-            port=PORT,
-            use_colors=False,
-            host="0.0.0.0",  # NOTE: Render requires you to bind your webserver to 0.0.0.0
-        )
-    )
-
-    # Run application and webserver together
-    async with application:
-        await application.start()
-        await webserver.serve()
-        await application.stop()
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
+    # Запуск бота
+    application.run_polling()
